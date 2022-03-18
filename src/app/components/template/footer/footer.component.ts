579051ca57a01dd9f936d6979a6cb6eb012765b5
Gustavo Leite <gustavo.leite170@gmail.com>
2022-03-17 23:36:05 -0300

    footer successfully done

--- src/app/components/template/footer/footer.component.ts
+++ src/app/components/template/footer/footer.component.ts
@@ -12,4 +12,24 @@ export class FooterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  selectTab ($event: any, $selector: any, $selectorTab: any, $city1: any, $city1Tab: any, $city2: any, $city2Tab: any): void {
+
+    $event.stopPropagation();
+
+    $selector.classList.add('selected');
+    $selectorTab.classList.add('selected');
+    
+    if($city1.className.indexOf('selected') !== -1) $city1.classList.remove('selected');
+    if($city1Tab.className.indexOf('selected') !== -1) $city1Tab.classList.remove('selected');
+
+    if($city2.className.indexOf('selected') !== -1) $city2.classList.remove('selected');
+    if($city2Tab.className.indexOf('selected') !== -1) $city2Tab.classList.remove('selected');
+
+    
+
+    /* switch($selector){
+      case(saoPaulo)
+    } */
+  }
+
 }

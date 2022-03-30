124c2592f56d06e67a6d8165a040cfa91343aea0
Gustavo Leite <gustavo.leite170@gmail.com>
2022-03-30 18:38:48 -0300

    homepage done with ngFor

--- src/app/components/views/homepage/homepage.component.ts
+++ src/app/components/views/homepage/homepage.component.ts
@@ -1,3 +1,4 @@
+import { ArrayType } from '@angular/compiler';
 import { Component, OnInit} from '@angular/core';
 
 @Component({
@@ -34,7 +35,52 @@ export class HomepageComponent implements OnInit {
     title: "Destaque do Ano do Anuário Informática",
     text: `MAGNA SISTEMAS escolhida no segmento de mercado Integradores - Médio Porte.`,
     button: "VEJA MAIS"
-  }, ]
+  }]
+
+  sec2: Array<any> = [{
+    link: "https://www.magnasistemas.com.br/wps/portal/internet/parceiros",
+    image: "https://www.magnasistemas.com.br/wps/wcm/connect/c17dc140-cae4-4475-9981-4e45a97bb71a/home06.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-c17dc140-cae4-4475-9981-4e45a97bb71a-leAWnzx",
+    title: "Parceiros",
+    text: `A MAGNASISTEMAS mantém parcerias estratégicas com as principais
+    empresas mundiais de tecnologia para o desenvolvimento de suas
+    soluções.`,
+    button: "CONHEÇA"
+  }, {
+    link: "https://jobs.kenoby.com/magnasistemas",
+    image: "https://www.magnasistemas.com.br/wps/wcm/connect/9983867d-e0ce-4784-b3d3-d36b8157a397/home05.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-9983867d-e0ce-4784-b3d3-d36b8157a397-leAWnzx",
+    title: "Carreira",
+    text: `Mais que colaboradores, pessoas! Conheça mais sobre a MAGNASISTEMAS
+    e nossas vagas! Venha fazer parte do nosso time!`,
+    button: "CONHEÇA"
+  }]
+
+  sec3: Array<any> = [{
+      link: "https://www.magnasistemas.com.br/wps/portal/internet/solucoes/detalhe/0bd2e0c4-2f99-426a-8194-901e2c1910e5",
+      image: "https://www.magnasistemas.com.br/wps/wcm/connect/2eaa2971-6de7-4e19-b79e-117268d440d6/Zeladoria.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-2eaa2971-6de7-4e19-b79e-117268d440d6-leAWnzx",
+      title: "Zeladoria Urbana Inteligente",
+      text: `Pessoas mais engajadas, cidades mais acolhedoras.`,
+      button: "CONHEÇA"
+    }, {
+      link: "https://www.magnasistemas.com.br/wps/portal/internet/solucoes/detalhe/fbfa4b95-c51f-4188-9e5a-f3c56f437806",
+      image: "https://www.magnasistemas.com.br/wps/wcm/connect/5ec56934-3472-49e4-8a09-e52252f14d15/CCI1b-MSWebsite.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-5ec56934-3472-49e4-8a09-e52252f14d15-leAWnzx",
+      title: "Centro de Controle Inteligente",
+      text: `Solução de Smarter Cities para controle, monitoramento e operação
+      da situação.`,
+      button: "CONHEÇA"
+    }, {
+      link: "https://www.magnasistemas.com.br/wps/portal/internet/solucoes/detalhe/5110b7c3-a52e-4431-95c6-6d3910dd6461",
+      image: "https://www.magnasistemas.com.br/wps/wcm/connect/38400a0d-e8d0-4d23-9d50-441668d25534/IoT+MSWebsite.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-38400a0d-e8d0-4d23-9d50-441668d25534-leAWnzx",
+      title: "Internet of Things",
+      text: `Soluções inteligentes para gestão integrada de ativos corporativos.`,
+      button: "CONHEÇA"
+    }, {
+      link: "https://www.magnasistemas.com.br/wps/portal/internet/solucoes/detalhe/c1e2af06-8f53-488c-9492-b4be4e1d4bdc",
+      image: "https://www.magnasistemas.com.br/wps/wcm/connect/3c6387f4-8cfa-4693-a3f0-cf66479ee601/BPMRPA-MSWebsite.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-3c6387f4-8cfa-4693-a3f0-cf66479ee601-leAWnzx",
+      title: "Automação de Processos",
+      text: `Modelagem, integração, otimização e gestão de processos de
+      negócio.`,
+      button: "CONHEÇA"
+  }]
 
   ngOnInit(): void {
   }
